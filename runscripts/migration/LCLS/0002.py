f1aa7f122e691f7268ad9342dc3c159f80f2e3e0
Murali Shankar <mshankar@slac.stanford.edu>
2020-03-25 16:28:49 -0700

    Clean up LCLS copyroles
    Trying to keep consistency between new and old experiments.
    Copy over the group memberships from LDAP
    Remove instrument permissions

--- runscripts/migration/LCLS/0002.py
+++ runscripts/migration/LCLS/0002.py
@@ -9,12 +9,11 @@ So, both database and LDAP have the group membership explicitly listed.
 import os
 import logging
 import argparse
+import re
 
 from pymongo import MongoClient
 from flask_authnz import UserGroups
 
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
 
 MONGODB_HOST=os.environ.get('MONGODB_HOST', "localhost")
 MONGODB_PORT=int(os.environ.get('MONGODB_PORT', 27017))
@@ -26,9 +25,16 @@ logbookclient = MongoClient(host=MONGODB_HOST, username=MONGODB_USERNAME, passwo
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Migrate group memberships from LDAP to the database')
+    parser.add_argument('--verbose', action="store_true")
     parser.add_argument('--dryrun', action="store_true")
     parser.add_argument('experiment', help='Specify experiment name; to run this for all experiments, use "all"')
     args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
 
     experiments = []
     def __check_and_add__(experiment_name):
@@ -56,9 +62,44 @@ if __name__ == '__main__':
         logger.debug("Migrating LDAP memberships for %s", experiment)
         expdb = logbookclient[experiment]
         for role in expdb["roles"].find({"players": experiment}):
-            logger.info("Found role %s for experiment %s", role["name"], experiment)
-            for uid in usergroups.get_group_members(experiment):
-                logger.info("Adding user %s to role %s for experiment %s", "uid:"+uid, role["name"], experiment)
-                if args.dryrun:
+            logger.debug("Found role %s for experiment %s", role["name"], experiment)
+            try:
+                grps = usergroups.get_group_members(experiment)
+            except KeyError as ke:
+                if ke.args[0] == "memberUid":
+                    logger.debug("Empty group membership for %s ", experiment)
                     continue
-                expdb["roles"].update_one({"app": role["app"], "name": role["name"]}, {"$addToSet": {"players": "uid:"+uid}})
+                else:
+                    raise ke
+            except Exception as ex:
+                raise ex
+            if isinstance(grps, list):
+                for uid in usergroups.get_group_members(experiment):
+                    if len(uid) < 3:
+                        logger.error("The experiment group %s probably has only one member; too small a userid %s", experiment, uid)
+                        continue
+                    logger.info("Adding user %s to role %s for experiment %s", "uid:"+uid, role["name"], experiment)
+                    if args.dryrun:
+                        continue
+                    expdb["roles"].update_one({"app": role["app"], "name": role["name"]}, {"$addToSet": {"players": "uid:"+uid}})
+            elif isinstance(grps, str):
+                    uid = grps
+                    if len(uid) < 3:
+                        logger.error("The experiment group %s probably has only one member; too small a userid %s", experiment, uid)
+                        continue
+                    logger.info("Adding user %s to role %s for experiment %s", "uid:"+uid, role["name"], experiment)
+                    if args.dryrun:
+                        continue
+                    expdb["roles"].update_one({"app": role["app"], "name": role["name"]}, {"$addToSet": {"players": "uid:"+uid}})
+            else:
+                raise Exception("Unexpected type for roles %s", type(grps))
+
+        for role in expdb["roles"].find({"players": {"$regex": re.compile("^ps-.*")}}):
+            instr_players = [x for x in role["players"] if x.startswith("ps-")]
+            if not instr_players:
+                raise Exception("Mongo and python disagree.")
+                continue
+            logger.info("Removing instrument roles %s for role %s for experiment %s", ",".join(instr_players), role["name"], experiment)
+            if args.dryrun:
+                continue
+            expdb["roles"].update_one({"app": role["app"], "name": role["name"]}, {"$pull": {"players": {"$in": instr_players }}})
