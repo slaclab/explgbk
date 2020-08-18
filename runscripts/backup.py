6a1d4205ce19ab4a2042b97d7b7d2fa22002aa62
Murali Shankar <mshankar@slac.stanford.edu>
2020-08-18 13:54:53 -0700

    Clean up backup..
    Giving up on incremental backup for now.
    Can't figure out a foolproof way to determine if a database has changed.
    We may need to do something like process the application logs and so on.
    While that seems hacky, it's probably more reliable than any other scheme.
    And it's probably applicable to calib/config/captar as well.
    So for now, we follow Knuth's advice and do not prematurely optimize. That comes later.

--- runscripts/backup.py
+++ runscripts/backup.py
@@ -1,23 +1,11 @@
 #!/usr/bin/env python
 '''
-Script for backing up the logbook.
-Meant to run as a cron job (perhaps daily) on the machines that have read access to the shard/replica sets storage.
-One can potentially run this on all machines with access to a shared's replica set.
-Run these at staggered times; the timestamp comparision will ensure that we do not backup a database multiple times in a time period.
-The staggering will ensure that one's replica set's backups will not mess up the other replica set's backups.
-
-This script relies on these settings in the mongod.conf for the shards
-storage:
-  engine: wiredTiger
-  directoryPerDB: true
-The directoryPerDB setting creates a folder for each database(or experiment) with the files for the various collections restricted to that folder.
-We determine the most recent timestamp for all the files in a database folder and use that as a proxy for the database modified timestamp.
-We initiate a backup if the database's most recent timestamp is more recent than the latest backup timestamp.
+Script for backing up the logbook/mongo.
 
 Note regarding the imagestores: We're deprecating support for external image stores; so only the mongo image stores are backed up.
 The file based image stores are expected to be backed up to tape as part of the experimental data backup.
 
-Backups are stored in a experiment per folder format with the filename of the backup based on the time the backup was initiated.
+Backups are stored in a database per folder format with the filename of the backup based on the time the backup was initiated.
 '''
 
 import os
@@ -72,52 +60,43 @@ def find_latest_timestamp_in_folder(folder):
         return datetime.datetime.fromtimestamp(os.path.getmtime(latest_file))
     return None
 
-def backup_experiment(args, database_name):
+def backup_database(args, database_name):
     logger.debug("Looking to backup for %s", database_name)
     db_backup_folder = os.path.join(args.backup_folder, database_name)
     pathlib.Path(db_backup_folder).mkdir(parents=True, exist_ok=True)
     archive_file_name = os.path.join(db_backup_folder, datetime.datetime.now().strftime(DATETIME_FILE_NAME_FORMAT) + ".gz")
 
-    latest_backup_ts = find_latest_timestamp_in_folder(db_backup_folder)
-    if latest_backup_ts and args.dbpath is not None:
-        logger.debug("We have a backup and a dbpath. Checking the dbpath to see if we need to do a backup")
-        latest_mongo_file_ts = find_latest_timestamp_in_folder(os.path.join(args.dbpath, database_name.replace("-", ".45")))
-        if not latest_mongo_file_ts:
-            logger.error("Cannot seem to find any files in the dbpath folder %s for database %s", os.path.join(args.dbpath, database_name), database_name)
-            sys.exit(-1)
-        if latest_backup_ts > latest_mongo_file_ts:
-            logger.info("%s - we already have a backup %s more recent than the database %s", database_name, latest_backup_ts, latest_mongo_file_ts)
-            return
-
     # We need to make a backup. Check to see if we need to delete an older backup
-    if args.keep_backups > 1:
+    if args.keep_backups != -1:
         logger.debug("Keeping at most %s backups", args.keep_backups)
         list_of_files = glob.glob(os.path.join(db_backup_folder, "*.gz"))
         if len(list_of_files) > args.keep_backups:
-            earliest_file = min(list_of_files, key=os.path.getmtime)
-            if earliest_file:
-                logger.info("%s - Removing earliest file %s for database", database_name, earliest_file)
-                os.remove(earliest_file)
+            for rmi in range(len(list_of_files) - args.keep_backups):
+                list_of_files = glob.glob(os.path.join(db_backup_folder, "*.gz"))
+                earliest_file = min(list_of_files, key=os.path.getmtime)
+                if earliest_file:
+                    logger.info("%s - Removing earliest file %s for database", database_name, earliest_file)
+                    os.remove(earliest_file)
+
+    uriwithdb = args.mongo_uri.split("?")[0] + database_name + "?" + args.mongo_uri.split("?")[1]
 
     logger.info("%s - New archive %s", database_name, archive_file_name)
     try:
         mdargs = [ args.mongodump_path,
             "-vv",
-            "--host", args.mongo_host,
-            "--port", str(args.mongo_port),
-            "--username", args.mongo_user,
-            "--password", args.mongo_password,
-            "--authenticationDatabase", args.mongo_auth_db,
-            "--db", database_name,
+            "--uri", uriwithdb,
             "--gzip",
             "--archive=" + archive_file_name
             ]
         mdp = subprocess.run(mdargs, check=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding="utf-8")
-        logger.debug(mdp.stdout)
-        logger.debug(mdp.stderr)
         if mdp.returncode != 0:
-            logger.error(mdp.stdout)
             logger.error("mongodump command - %s - returned non-zero error code %s", " ".join(mdargs), mdp.returncode)
+            logger.error(mdp.stdout)
+            logger.error(mdp.stderr)
+        else:
+            logger.debug(mdp.stdout)
+            logger.debug(mdp.stderr)
+
     except:
         logger.exception("Exception dumping database to %s", archive_file_name)
         sys.exit(-1)
@@ -126,34 +105,25 @@ def backup_experiment(args, database_name):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--verbose",     action='store_true', help="Turn on verbose logging")
-    parser.add_argument("--mongo_host",        help="The hostname of the mongodb server. In sharded instances, this should point to the mongos router.", required=True)
-    parser.add_argument("--mongo_port",        help="The port to the mongodb/mongos server.", default=27017, type=int)
-    parser.add_argument("--mongo_user",        help="The username used for connecting to the Mongo server; what works is giving this user the backup role in the admin database.", required=True)
-    parser.add_argument("--mongo_password",    help="The password for the user.", required=True)
-    parser.add_argument("--mongo_auth_db",     help="The authentication database for the user. Since this user is one that spans databases; we probably need the user to come from the admin database", default="admin")
+    parser.add_argument("--mongo_uri",         help="The MongoURI to use; this seems the best way to specify some useful options including readPreference=secondary. This needs to be passed in a certain way for mongodump to work; that is, the path part should end with a / followed immediately by a ? with the options. For example, mongodb://backupuser:backuppassword@localhost:27017/?authSource=admin&readPreference=secondary", required=True)
     parser.add_argument("--mongodump_path",    help="Full path to the mongodump command. If not specified, we use mongodump from the PATH.", default="mongodump")
-    parser.add_argument("--keep_backups",      help="Keep at least this many complete backups; older backups are deleted.", default=0, type=int)
-    parser.add_argument("--dbpath",            help="The db path to the shard/replica set we are trying to backup. Note, we assume that directoryPerDB is set and we have a folder per database.")
+    parser.add_argument("--keep_backups",      help="Keep at least this many complete backups; older backups are deleted.", default=-1, type=int)
     parser.add_argument("backup_folder",       help="The folder containing the backups.")
 
     args = parser.parse_args()
     configureLogging(args.verbose)
     test_executable_exists([args.mongodump_path, "--help"])
 
-    logbookclient = MongoClient(
-        host=args.mongo_host,
-        port=args.mongo_port,
-        username=args.mongo_user,
-        password=args.mongo_password,
-        authSource=args.mongo_auth_db,
-        tz_aware=True)
+    if not os.path.exists(args.backup_folder) or not os.path.isdir(args.backup_folder):
+        logger.error("The root folder for database backups %s does not seem to exist", args.backup_folder)
+        sys.exit(-1)
 
-    backup_experiment(args, "site")
+    logbookclient = MongoClient(args.mongo_uri, tz_aware=True)
 
-    logger.info("Gathering the list of experiments")
+    logger.info("Gathering the list of databases")
     database_names = sorted(list(logbookclient.list_database_names()))
-    for experiment_name in database_names:
-        if experiment_name in ["admin", "config", "local"]:
-            logger.info("Not backing up %s", experiment_name)
+    for database_name in database_names:
+        if database_name in [ "admin", "config", "local" ]:
+            logger.info("Not backing up system database %s", database_name)
             continue
-        backup_experiment(args, experiment_name)
+        backup_database(args, database_name)
