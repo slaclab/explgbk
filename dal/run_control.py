ec0286415b0910d867dea1da4144272e6cb0de50
Murali Shankar <mshankar@slac.stanford.edu>
2020-09-16 14:50:05 -0700

    Use the primary alone for run control.

--- dal/run_control.py
+++ dal/run_control.py
@@ -9,7 +9,7 @@ import re
 
 import requests
 
-from pymongo import ASCENDING, DESCENDING, ReturnDocument
+from pymongo import ASCENDING, DESCENDING, ReturnDocument, ReadPreference
 from bson import ObjectId
 
 from context import logbookclient
@@ -25,7 +25,7 @@ def start_run(experiment_name, run_type, user_specified_run_number=None, user_sp
     Start a new run for the specified experiment
     If the user_specified_run_number is not specified; we use the next_runnum autoincrement counter.
     '''
-    expdb = logbookclient[experiment_name]
+    expdb = logbookclient.get_database(experiment_name, read_preference=ReadPreference.PRIMARY)
     if not user_specified_run_number:
         next_run_num_doc = expdb['counters'].find_one_and_update({ "_id" : "next_runnum"}, {'$inc': {'seq': 1}}, return_document=ReturnDocument.AFTER)
         if not next_run_num_doc:
@@ -58,7 +58,7 @@ def get_current_run(experiment_name):
     '''
     Get the run document for the run with the maximum run number.
     '''
-    expdb = logbookclient[experiment_name]
+    expdb = logbookclient.get_database(experiment_name, read_preference=ReadPreference.PRIMARY)
     current_run_doc = expdb.runs.find().sort([("num", DESCENDING)]).limit(1)
     return list(current_run_doc)[0] if current_run_doc.count() else None
 
@@ -66,7 +66,7 @@ def get_run_doc_for_run_num(experiment_name, run_num):
     """
     Get the run document for the specified run number
     """
-    expdb = logbookclient[experiment_name]
+    expdb = logbookclient.get_database(experiment_name, read_preference=ReadPreference.PRIMARY)
     run_doc = expdb.runs.find_one({"num": run_num})
     if run_doc:
         return run_doc
@@ -159,7 +159,7 @@ def end_run(experiment_name, user_specified_end_time=None):
     '''
     End the current run; this is mostly a matter of filling in the end time
     '''
-    expdb = logbookclient[experiment_name]
+    expdb = logbookclient.get_database(experiment_name, read_preference=ReadPreference.PRIMARY)
     current_run_doc = get_current_run(experiment_name)
     end_time = user_specified_end_time if user_specified_end_time else datetime.datetime.utcnow()
     return expdb.runs.find_one_and_update({"num": current_run_doc["num"]}, {'$set': {'end_time': end_time}}, return_document=ReturnDocument.AFTER)
@@ -168,7 +168,7 @@ def is_run_closed(experiment_name, run_num):
     '''
     Check if the specified run is closed
     '''
-    expdb = logbookclient[experiment_name]
+    expdb = logbookclient.get_database(experiment_name, read_preference=ReadPreference.PRIMARY)
     run_doc = expdb.runs.find_one({"num": run_num})
     if run_doc and run_doc.get('end_time', None):
         return True
