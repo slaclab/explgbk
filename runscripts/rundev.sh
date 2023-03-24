5e02890f1b036d0b6a66e0f6969ca1c186945b73
Murali Shankar <mshankar@slac.stanford.edu>
2023-03-24 10:12:10 -0700

    Clean up start script

--- runscripts/rundev.sh
+++ runscripts/rundev.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-#export PATH=/reg/g/psdm/sw/conda/inst/miniconda2-prod-rhel7/bin:${PATH}
-#source activate ana-1.2.12-py3
-
-# Use this pattern to setup configuration.
-# However, please use the EXTERNAL_CONFIG_FILE that is NOT part of this repo to set up sensitive/installation-specific config.
-# [ -z "$ROLES_DATABASE_HOST" ] && export ROLES_DATABASE_HOST="localhost"
-
 # We pick up installation-specific config from a file outside of this repo.
 PRNT_DIR=`dirname $PWD`
 G_PRNT_DIR=`dirname $PRNT_DIR`;
 GG_PRNT_DIR=`dirname $G_PRNT_DIR`;
 GGG_PRNT_DIR=`dirname $GG_PRNT_DIR`;
 EXTERNAL_CONFIG_FILE="${GGG_PRNT_DIR}/appdata/explgbk_config/explgbk_config.sh"
-
 
 if [[ -f "${EXTERNAL_CONFIG_FILE}" ]]
 then
@@ -22,7 +14,6 @@ then
 else
    echo "Did not find external deployment specific configuration - ${EXTERNAL_CONFIG_FILE}"
 fi
-
 
 export ACCESS_LOG_FORMAT='%(h)s %(l)s %({REMOTE_USER}i)s %(t)s "%(r)s" "%(q)s" %(s)s %(b)s %(D)s'
 
