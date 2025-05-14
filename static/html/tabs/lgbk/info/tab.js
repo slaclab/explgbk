6fcc0b7e3ea8845273b1143d537373dc6635a369
Murali Shankar <mshankar@slac.stanford.edu>
2025-05-14 08:39:36 -0700

    Support overriding the questionnaire URL prefix from instrument definition
    For example, for the ued instrument define a instrument custom param
    questionnaire_prefix: '../../../ued_questionnaire_slac'

--- static/html/tabs/lgbk/info/tab.js
+++ static/html/tabs/lgbk/info/tab.js
@@ -1,21 +1,24 @@
 export function tabshow(target) {
     const baseUrl = target.getAttribute("data-lg-url").split("/static")[0];
     const infoUrl = baseUrl + "/lgbk/"+experiment_name+"/ws/info";
+    const instrumentsUrl = baseUrl + "/lgbk/ws/instruments";
     const templateUrl = baseUrl + "/static/html/ms/info_" + logbook_site + ".html";
     console.log("Inside tab show for sessions " + infoUrl);
-    Promise.all([fetch(new Request(infoUrl)), fetch(new Request(templateUrl))])
+    Promise.all([fetch(new Request(infoUrl)), fetch(new Request(templateUrl)), fetch(new Request(instrumentsUrl))])
     .then((resps) => {
-        return Promise.all([resps[0].json(), resps[1].text()]);
+        return Promise.all([resps[0].json(), resps[1].text(), resps[2].json()]);
     })
     .then((vals) => {
         const tabpanetmpl = `<div class="container-fluid text-center tabcontainer" id="info_tab"><div class="row content"><div class="lgcontent col-12"></div></div></div>`;
-        let [ inforesp, tmpl ]= vals;
+        let [ inforesp, tmpl, instruments ]= vals;
         let trgtname = target.getAttribute("data-bs-target"), trgt = document.querySelector(trgtname); 
         trgt.innerHTML=tabpanetmpl;
 
         const info = inforesp.value, contact_info = info.contact_info, parts = contact_info.split(')')[0].split('(');
         info.contact_name = parts[0];
         info.contact_email = parts[1];
+        const instrument = _.get(info, "instrument");
+        info.instrument_info = _.find(instruments.value, ["_id", instrument]);
         Mustache.parse(tmpl);
         $("#info_tab").find(".lgcontent").empty().append($(Mustache.render(tmpl, {info: info, FormatDate: elog_formatdatetime})));
         $("#info_tab").trigger("info_loaded", info);
