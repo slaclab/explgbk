0776d4f2237c732631d4f2ebbc153dc84276b151
Samuel Welborn <swelborn@stanford.edu>
2026-04-07 20:36:34 -0400

    add tables

---
+++ explgbk2/backend/app/models/v2/base.py
@@ -0,0 +1,15 @@
+from uuid import UUID
+
+AttachmentID = UUID
+LogbookID = UUID
+EntryID = UUID
+EntryRevisionID = UUID
+UserUUID = UUID
+TagID = UUID
+ShiftID = UUID
+RunID = UUID
+InstrumentID = UUID
+ExperimentID = UUID
+ProposalID = UUID
+ExternalLinkID = UUID
+LegacyMongoID = str
