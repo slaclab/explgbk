34b001d1b05168e899f2a91ac69f76c5c68d1a40
Murali Shankar <mshankar@slac.stanford.edu>
2026-09-10 15:23:35 -0700

    Remove the proposal abstract for the open endpoint

--- services/explgbk.py
+++ services/explgbk.py
@@ -725,6 +725,8 @@ def svc_lookup_experiment_in_URAWI():
     run_period = request.args.get("run_period", None)
     urawi_doc = get_ques_proposal_details(experiment_name, run_period, proposal_id)
     if urawi_doc and urawi_doc.get("URAWI", {}) and urawi_doc["URAWI"].get("proposalNo", None):
+        urawi_doc.get("URAWI", {}).pop("proposalAbstract", None)
+        urawi_doc.get("UPS", {}).pop("Proposal_Abstract", None)
         return jsonify({'success': True, "value": urawi_doc})
     return jsonify({'success': False})
 
